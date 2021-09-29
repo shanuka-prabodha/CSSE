0e7b23a71b0a181f76a17b4c1b53a960b2e3093e
Dinisuru <641501719v>
2021-09-30 01:26:09 +0530

    order view working

--- WebApp/Frontend/src/MyComponents/sidebar/side.js
+++ WebApp/Frontend/src/MyComponents/sidebar/side.js
@@ -18,9 +18,10 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import Test from '../../Pages/Test';
-import Test3 from '../../Pages/Test3';
+import OneOrder from '../../Pages/Orders/GetOneOrder/OneOrder';
 import ButtonBar from '../ButtonBar/ButtonBar';
 import PendingOrders from '../../Pages/PendingOrders';
+import { useHistory } from "react-router-dom";
 
 const drawerWidth = 240;
 
@@ -92,7 +93,9 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 export default function MiniDrawer() {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
-  const [mytab , SetTab] = React.useState("1")
+  const [mytab, SetTab] = React.useState("1")
+  let history = useHistory();
+
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -101,6 +104,19 @@ export default function MiniDrawer() {
   const handleDrawerClose = () => {
     setOpen(false);
   };
+
+  const linksto = [
+    { label: 'REQUUEST', value: "/request" },
+    { label: 'APPROVAL', value: '/approval' },
+    { label: 'RECEVIED', value: '/recevied' },
+    { label: 'WAREHOUSE', value: '/warehouse' },
+    { label: 'SETTINGS', value: '/settings' },
+    { label: 'Home', value: '/' }
+  ]
+
+
+
+
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -132,14 +148,16 @@ export default function MiniDrawer() {
         </DrawerHeader>
         <Divider />
         <List>
-          {['REQUUEST', 'APPROVAL', 'RECEIVED', 'WAREHOUSE', 'SETTINGS'].map((text, index) => (
-            <ListItem button key={text} onClick={e=>{
-                SetTab(index)
+          {linksto.map((text, index) => (
+            <ListItem button key={text} onClick={e => {
+
+              history.push(text['value']);
+
             }}>
               <ListItemIcon>
                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={text["label"]} />
             </ListItem>
           ))}
         </List>
@@ -147,9 +165,9 @@ export default function MiniDrawer() {
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
-            {mytab==0?<Test/>:mytab==1?<Test3/>:<PendingOrders/>}
-                
 
+         <PendingOrders/>
+         
       </Box>
     </Box>
   );
